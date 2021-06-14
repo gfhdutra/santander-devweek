49db9766b616eeaecff804c76a15a4048d241419
Gabriel Dutra <gfhdutra@gmail.com>
2021-06-14 19:55:59 -0300

    initial commit

---
+++ src/main/java/com/project/devweek/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package com.project.devweek.exceptions;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
